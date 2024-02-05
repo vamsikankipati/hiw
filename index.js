63de6a28b79f38ec7db81399b4bb660796d483b7
Vamsi Kankipati <vamsibh1@gmail.com>
2024-02-05 19:12:18 +0530

    update

--- index.js
+++ index.js
@@ -3,7 +3,13 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/app/App';
 import {name as appName} from './app.json';
+// import { PaperProvider } from 'react-native-paper';
 
-AppRegistry.registerComponent(appName, () => App);
+export const Main = () => {
+  // eslint-disable-next-line react/react-in-jsx-scope
+  return <App />;
+};
+
+AppRegistry.registerComponent(appName, () => Main);
---
+++ src/app/Menu/Menu.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors
+} from 'react-native/Libraries/NewAppScreen';
+
+const styles = StyleSheet.create({
+    sectionContainer: {
+      marginTop: 32,
+      paddingHorizontal: 24,
+    },
+  });
+
+function Menu(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Text style={styles.sectionContainer}>
+            This is menu section
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default Menu;
